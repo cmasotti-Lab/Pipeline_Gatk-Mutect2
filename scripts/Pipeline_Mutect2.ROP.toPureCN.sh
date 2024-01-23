632f717cb041884419c7474e7aefe017f662f8d9
vandeclecio <clecio.de@gmail.com>
2024-01-22 22:23:02 -0300

    Update Pipeline_Mutect2.ROP.toPureCN.sh
    scritp toPureCN atualizado

--- scripts/Pipeline_Mutect2.ROP.toPureCN.sh
+++ scripts/Pipeline_Mutect2.ROP.toPureCN.sh
@@ -17,7 +17,7 @@ export SCRATCH60="/home/scratch60/vlira_18jan2024/"
 
 export DATA=$(date "+%F")
 export MEM=50
-export JOBS=5
+export JOBS=10
 #export DATA='2024-01-19'   # EDITE AQUI SE QUISER USAR UMA PASTA DE UMA DATA ESPECIFICA
 
 export OUTPUT_DIR=${SCRATCH60}"/Result_Mutect2.ROP.toPureCN.${DATA}"
@@ -25,7 +25,7 @@ export OUTPUT_DIR=${SCRATCH60}"/Result_Mutect2.ROP.toPureCN.${DATA}"
 export SAMPLES_DIR="$SCRATCH60/preprocessing_FINAL_result/"
 export SAMPLES_FILE=$(find "$SAMPLES_DIR" -maxdepth 1 -mindepth 1  -name '*.dedup.tags.bqsr.bam')
 export REF_FASTA="/home/projects2/LIDO/molPathol/oncoseek/nextseq/hg38/"
-export GATK="$SCRATCH60/tools/gatk-4.5.0.0/./gatk"
+export GATK="$SCRATCH60/tools/gatk-4.3.0.0/./gatk"
 #export GATK="gatk"
 export TARGET="$SCRATCH60/references/xgen-exome-research-panel-v2-targets-hg38.bed"
 export PON="/home/users/vlira/PanelOfNornal/PoN.100COVID.vcf.gz"
@@ -35,7 +35,7 @@ export ANNOVAR="$SCRATCH60/tools/annovar/table_annovar.pl"
 export ANNOVAR_DB="$SCRATCH60/humandb/"
 export CROSS_REFERENCE="$SCRATCH60/references/refGene_TARGET_COSMICv82CensusGene_F1.txt"
 
-export TIME_FILE="$OUTPUT_DIR.log"
+local TIME_FILE="$OUTPUT_DIR.log"
 
 mkdir $OUTPUT_DIR
 find "$SAMPLES_DIR" -maxdepth 1 -mindepth 1  -name '*.dedup.tags.bqsr.bam.cram' > $OUTPUT_DIR/samples.list
@@ -44,28 +44,30 @@ stage_Mutect2 (){
   local SAMPLE=$1
   NAME="${SAMPLE##*/}"
   echo "" >> $TIME_FILE
-  echo ">>>>>> Executando stage_Mutect2 para Amostra: "$NAME" <<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando stage_Mutect2 para Amostra: "$NAME"  <<< " >> $TIME_FILE
   date >> $TIME_FILE
 
-    samtools index /home/scratch60/vlira_18jan2024/preprocessing_FINAL_result/${NAME}
+   # samtools index /home/scratch60/vlira_18jan2024/preprocessing_FINAL_result/${NAME}
 
     $GATK --java-options "-Xmx100G" Mutect2 \
-      --genotype-germline-sites true --genotype-pon-sites true \
-      -R $REF_FASTA/Homo_sapiens_assembly38.fasta \
-      -L $TARGET \
-      -I $SAMPLE \
-      --germline-resource $GNOMAD \
-      --panel-of-normals $PON   \
-      --f1r2-tar-gz $OUTPUT_DIR/Mutect2/$NAME.f1r2.tar.gz \
-      -bamout $OUTPUT_DIR/Mutect2/$NAME.bamout.bam \
-      -O $OUTPUT_DIR/Mutect2/$NAME.unfiltered.vcf.gz 2> $OUTPUT_DIR/Mutect2/$NAME.unfiltered.log
+          --genotype-germline-sites true --genotype-pon-sites true \
+          -R $REF_FASTA/Homo_sapiens_assembly38.fasta \
+          -L $TARGET \
+          -I $SAMPLE \
+          --germline-resource $GNOMAD \
+          --panel-of-normals $PON   \
+          --f1r2-tar-gz $OUTPUT_DIR/Mutect2/$NAME.f1r2.tar.gz \
+          -bamout $OUTPUT_DIR/Mutect2/$NAME.bamout.bam \
+          -O $OUTPUT_DIR/Mutect2/$NAME.unfiltered.vcf.gz 2> $OUTPUT_DIR/Mutect2/$NAME.unfiltered.log
 }
 export -f stage_Mutect2
 
     
 stage_LearnReadOrientationModel (){
   echo "" >> $TIME_FILE
-  echo ">>>>>> STAGE_LearnReadOrientationModel<<<<<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> STAGE_LearnReadOrientationModel<<<<<<" >> $TIME_FILE
   date >> $TIME_FILE
 
   local SAMPLE_READORIENTATION=$(find "$OUTPUT_DIR"/Mutect2/ -maxdepth 1 -mindepth 1  -name '*.f1r2.tar.gz')
@@ -81,7 +83,8 @@ stage_GetPileupSummaries (){
   local SAMPLE=$1
   NAME="${SAMPLE##*/}"
   echo "" >> $TIME_FILE
-  echo ">>>>>> Executando GetPileupSummaries para Amostra: "$NAME" <<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando GetPileupSummaries para Amostra: "$NAME" <<<" >> $TIME_FILE
   date >> $TIME_FILE
 
   $GATK --java-options "-Xmx${MEM}G" GetPileupSummaries \
@@ -96,7 +99,8 @@ stage_CalculateContamination (){
   local SAMPLE=$1
   NAME="${SAMPLE##*/}"
   echo "" >> $TIME_FILE
-  echo ">>>>>> Executando CalculateContamination para Amostra: "$NAME" <<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando CalculateContamination para Amostra: "$NAME" <<<" >> $TIME_FILE
   date >> $TIME_FILE
  
   $GATK --java-options "-Xmx${MEM}G" CalculateContamination \
@@ -111,7 +115,8 @@ stage_FilterMutectCalls (){
   local SAMPLE=$1
   NAME="${SAMPLE##*/}"
   echo "" >> $TIME_FILE
-  echo ">>>>>> Executando FilterMutectCalls para Amostra: "$NAME" <<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando FilterMutectCalls para Amostra: "$NAME" <<<" >> $TIME_FILE
   date >> $TIME_FILE
  
   $GATK --java-options "-Xmx${MEM}G" FilterMutectCalls \
@@ -131,7 +136,8 @@ left_normalization () {
   local SAMPLE=$1
   NAME="${SAMPLE##*/}"
   echo "" >> $TIME_FILE
-  echo ">>>>>> Executando Normalization para Amostra: "$NAME" <<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando Normalization para Amostra: "$NAME" <<<" >> $TIME_FILE
   date >> $TIME_FILE
 
     bcftools norm -m-both -O z -o $OUTPUT_DIR/left_normalization/$NAME.norm_Step1.vcf.gz $OUTPUT_DIR/FilterMutectCalls/$NAME.filtered.vcf.gz 2> $OUTPUT_DIR/left_normalization/$NAME.norm_Step1.log
@@ -146,7 +152,8 @@ annotation (){
 
   # Merge all genotyped samples to get a single VCF/BCF using bcftools merge
   echo "" >> $TIME_FILE
-  echo ">>>>>> Executando Vcftools para todas juntar os vcf das amostras<<<<<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando Vcftools para todas juntar os vcf das amostras<<<<<<" >> $TIME_FILE
   date >> $TIME_FILE
   
   local SAMPLE_CALL_GENO=$(find "$OUTPUT_DIR"/left_normalization/ -maxdepth 1 -mindepth 1  -name '*.norm_Step2.vcf.gz')
@@ -165,7 +172,8 @@ annotation (){
 
   # # annovar
    echo "" >> $TIME_FILE
-   echo ">>>>>> Executando annovar para todas juntar os vcf das amostras<<<<<<" >> $TIME_FILE
+   local TIME=date
+   echo "${TIME} >>>>>> Executando annovar para todas juntar os vcf das amostras<<<<<<" >> $TIME_FILE
   
    $ANNOVAR  --vcfinput $OUTPUT_DIR/annotation/mutect.merged.norm_Step2.vcf.gz $ANNOVAR_DB -buildver hg38 --remove \
    --protocol refGene,avsnp147,gnomad_exome,abraom,cosmic95,icgc28,dbnsfp42a  \
@@ -178,7 +186,8 @@ annotation (){
   
 
   date >> $TIME_FILE
-  echo ">>>>>> Executando SnpSift para todas juntar os vcf das amostras<<<<<<" >> $TIME_FILE
+  local TIME=date
+  echo "${TIME} >>>>>> Executando SnpSift para todas juntar os vcf das amostras<<<<<<" >> $TIME_FILE
   
   java -jar -Xmx50G $SCRATCH60/tools/snpEff/SnpSift.jar extractFields  "$OUTPUT_DIR"/annotation/annovar.norm.hg38_multianno.correct.vcf \
     -e . "CHROM" "POS" "ID" "REF" "ALT" "FILTER" "AC" "AN" "DP" "Func.refGene" "Gene.refGene" "GeneDetail.refGene" "ExonicFunc.refGene" "AAChange.refGene" \
@@ -218,6 +227,6 @@ mkdir $OUTPUT_DIR/annotation/
 
 
 echo "" >> $TIME_FILE
-echo ">>>>>> End Pipeline <<< " >> $TIME_FILE
+echo "${TIME} >>>>>> End Pipeline <<< " >> $TIME_FILE
 date >> $TIME_FILE
 echo "" >> $TIME_FILE
