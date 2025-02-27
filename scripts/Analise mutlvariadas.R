8c41bd8fde8fe695e3be547d6af65e5845fcfa8b
vandeclecio <clecio.de@gmail.com>
2025-02-27 15:17:47 -0300

    Update Analise mutlvariadas.R

--- scripts/Analise mutlvariadas.R
+++ scripts/Analise mutlvariadas.R
@@ -1,6 +1,9 @@
 library("forestmodel")
 library(car)
 
+
+
+
 # CARREGANDO DADOS DA TABELA dados GENADOS NO SCRIPT ANTERIOR
 # Organizando os dados continuos em (numeric) 
 # E os dados categoricos em (factor)
